9032a9ab3729edbc7562281e3f8bc3ce2e208b93
mnalrefaey <m.n.alrefaey@gmail.com>
2023-06-16 07:01:33 -0700

    ff

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,6 @@
 #include <unistd.h>
  /**
-  * main - Entry point
+  * main-Entry point
   * description:using write function to print a quote
   * size_t write (int fd, const void *buf , size_t count);
   * Return: 1 (error)
