8db734ec1eee6ab47c38930708442e871b6f13c6
mnalrefaey <m.n.alrefaey@gmail.com>
2023-06-15 18:26:35 -0700

    hhfghdf

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,11 +6,10 @@
 */
 int main(void)
 {
-printf("size of a char:", sizeof(char) byte(s)\n);
-printf("size of a int:", sizeof(int) byte(s)\n);
-printf("size of a long int:", sizeof(long int) byte(s)\n);
-printf("size of a long long int:", sizeof(long long int) byte(s)\n);
-printf("size of a float:", sizeof(float) byte(s)\n);
-
+printf("size of a char: %lu byte(s)\n".sizeof(char));
+printf("size of an int: %lu byte(s)\n".sizeof(int));
+printf("size of a long int: %lu byte(s)\n".sizeof(long int));
+printf("size of a long long int: %lu byte(s)\n".sizeof(long long int));
+printf("size of a float: %lu byte(s)\n".sizeof(float));
 return (0);
 }
