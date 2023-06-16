ca1152533a892783d7992c49d73f0769bbf28d1d
mnalrefaey <m.n.alrefaey@gmail.com>
2023-06-15 17:47:13 -0700

    j

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * description:A c prog that prints with put function
+ * description:A c programming that prints with put function
  * Return:always 0 (sucsses)
 */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return (0);
+return(0);
 }
