67736f86764e91733dc7cc2fad56f69b1b23a799
mnalrefaey <m.n.alrefaey@gmail.com>
2023-06-16 17:57:00 -0700

    ff

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (success)
+*/
+int main(void)
+{
+int n = 0;
+while (n <= 9)
+{
+putchar (n + 48);
+if (n != 9)
+{
+putchar (',');
+putchar (' ');
+}
+n++;
+}
+return (0);
+}
