1134c366035c771a9e2df562249198deaedc9817
mnalrefaey <m.n.alrefaey@gmail.com>
2023-06-20 11:54:31 -0700

    yy

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,13 +3,13 @@
 /**
  * _abs - function that computes the absolute value of an intege
  *
- * @int: checks input of function
+ * @n: takes an integer for function
  *
  * Return: returns always(0) success
 */
-int _abs(int)
+int _abs(int n)
 {
 if (n < 0)
-n = n * -1;
+n = n * (-1);
 return (n);
 }
