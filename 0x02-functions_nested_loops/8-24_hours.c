1414f4ca5ea0e921fd6c971c19499db68ab94c69
mnalrefaey <m.n.alrefaey@gmail.com>
2023-06-20 13:30:26 -0700

    jj

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -4,12 +4,12 @@
 * jack_bauer- prints every minute of the day
 */
 
-void jack_bauer (void)
+void jack_bauer(void)
 {
 int minute, hour;
 for (hour = 0; hour <= 23; hour++)
 {
-for (minute = 0; minute <= 59; minute ++)
+for (minute = 0; minute <= 59; minute++)
 {
 _putchar((hour / 10) + 48);
 _putchar((hour % 10) + 48);
