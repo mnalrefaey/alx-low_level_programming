ba109ec7b0ff4569068fa8aeb2983a981b8217f6
mnalrefaey <m.n.alrefaey@gmail.com>
2023-06-20 07:22:51 -0700

    wq

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include"main.h"
+
+/**
+ * print_alphabet_x10 -  print the alphabet 10 times
+ *
+*/
+
+void print_alphabet_x10(void);
+{
+int ch, sh;
+for (ch = 0; ch <= 9; ch++)
+{
+for (sh = 'a'; sh <= 'z'; sh++)
+{
+_putchar(sh);
+}
+_putchar('\n');
+}
+}
