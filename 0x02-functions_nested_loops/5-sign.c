c3314e629118a2fcb6f5715cbe16bd142d92166a
mnalrefaey <m.n.alrefaey@gmail.com>
2023-06-20 11:18:49 -0700

    wq

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -14,15 +14,15 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		return (1);
-		_putchar('43');
+		_putchar(43);
 	}
 	else if (n == 0)
 	{
 		return (0);
-		_putchar('48');
+		_putchar(48);
 	}
 	else
 	{
 		return (-1);
-		_putchar('45');
+		_putchar(45);
 	}
