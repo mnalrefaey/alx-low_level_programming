f9864561a0ecea407c1f0ba72132fea7f04e3461
mnalrefaey <m.n.alrefaey@gmail.com>
2023-06-20 12:09:02 -0700

    tt

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+i#include"main.h"
+
+/**
+ * print_last_digit - function that prints the last digit
+ *
+ * @n: takes an integer for function
+ *
+ * Return: returns always(0) success
+*/
+
+int print_last_digit(int n);
+{
+int digit = n % 10;
+return (0);
+}
