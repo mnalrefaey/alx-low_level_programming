57eb39de8a573b6d18560ef5d55dec172f4ddbe0
mnalrefaey <m.n.alrefaey@gmail.com>
2023-06-21 08:39:21 -0700

    wwwwwwww

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -4,14 +4,15 @@
 /**
 * print_remaining_days - takes a date and prints how many days are
 * left in the year, taking leap years into account
-* @month: month in number format
-* @day: day of month
-* @year: year
-* Return: void
+* @month:the month in number format
+* @day:the day of month
+* @year:the year
+* Return:void
 */
 
 void print_remaining_days(int month, int day, int year)
 {
+
 if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
 {
 if (month >= 2 && day >= 60)
