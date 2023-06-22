8a028198eb46a1e9ccdfaa68ac9ac4ce8a252a88
mnalrefaey <m.n.alrefaey@gmail.com>
2023-06-22 16:24:42 -0700

    wq

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *print_numbers - a function that prints the numbers 0-9
+ *
+ *Return:void
+ */
+
+void print_numbers(void)
+{
+	char a;
+   for (a ='0' ; a <= '9'; a++)
+   {
+        _putchar (a);
+   }
+   _putchar('\n');
+}
