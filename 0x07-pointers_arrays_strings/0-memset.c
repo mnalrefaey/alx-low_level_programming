6011f7ee5415bbd527dac0d1c0742aec8165acb6
mnalrefaey <m.n.alrefaey@gmail.com>
2023-07-03 10:18:45 -0700

    ww

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,13 +10,11 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i;
+unsigned int i;
 
-	for (i = 0; n > 0; i++)
-	{
-		s[i] = b;
-		n--;
-	}
-	return (s);
+for (i = 0; i < n; i++)
+{
+s[i] = b;
 }
-
+return (s);
+}
---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _memcpy -  copies memory area
+ * @dest: starting address of memory to be filled
+ * @src: the desired value
+ * @n: number of bytes to be changed
+ *
+ * Return: changed array with new value for n bytes
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i;
+
+for (i = 0; i < n; i++)
+{
+dest[i] = src[i];
+}
+return (dest);
+}
