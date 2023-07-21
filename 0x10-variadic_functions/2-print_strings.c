67819d944207a5da06884a0820de1346a80ec1d9
mnalrefaey <m.n.alrefaey@gmail.com>
2023-07-20 17:07:34 -0700

    tt

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -12,15 +12,17 @@
 
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-unsigned int x;
 va_list count;
+char *str;
+unsigned int x;
 va_start(count, n);
 for (x = 0; x < n; x++)
 {
-if (va_arg(count, char*) == NULL)
+str = va_arg(count, char*);
+if (str == NULL)
 printf("nil");
 else
-printf("%s", va_arg(count, char*));
+printf("%s", str);
 if (x != (n - 1) && separator != NULL)
 printf("%s", separator);
 }
