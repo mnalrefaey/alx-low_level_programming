b24cf4c3dec685039dd2ae3de9c8a3290b574e05
mnalrefaey <m.n.alrefaey@gmail.com>
2023-07-28 06:52:52 -0700

    q

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,4 +1,5 @@
-#include "list.t"
+#include <stdio.h>
+#include "lists.h"
 
 /**
  * print_list - a function that prints all the elements of a list_t list
@@ -11,19 +12,15 @@
 size_t print_list(const list_t *h)
 {
 size_t count = 0;
-while (h != NULL)
+while (h)
 {
-printf("[%d] %s\n", _strlen(h->str), h->str ? h->str : "(nil)");
+if (!h->str)
+printf("[0] (nil)\n");
+else
+printf("[%u] %s\n", h->len, h->str);
 h = h->next;
 count++;
 }
 return (count);
-}
-
-
-
-
-
-
 }
 
