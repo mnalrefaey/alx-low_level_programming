4dab85fc318f8d067a6f5ced00af422dd8620563
mnalrefaey <m.n.alrefaey@gmail.com>
2023-07-31 16:35:20 -0700

    oo

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -8,7 +8,7 @@
  *@head:a pointer
  *@idx:index of the new node
  *@n:data
- * Return: new node or (NULL) if the list is empty
+ * Return: pointer to the new node or (NULL) if the list is empty
  */
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
@@ -16,14 +16,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 listint_t *tmp = *head;
 unsigned int i;
 listint_t *new;
-if (*head == NULL)
+
+new = malloc(sizeof(listint_t));
+if (new == NULL || head == NULL)
 {
 return (NULL);
 }
-new = malloc(sizeof(listint_t));
-if (new == NULL)
-return (NULL);
-new->next = *head;
+new->next = NULL;
 new->n = n;
 if (idx == 0)
 {
